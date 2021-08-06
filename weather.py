d546e01de5ed7518283ffe6d345485db45bbb488
dianaluna01 <72881310+dianaluna01@users.noreply.github.com>
2021-08-06 01:34:42 -0400

    Create weather.py

---
+++ weather.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+import requests
+import sqlite3
+
+app = Flask(__name__)
+
+class City():
+    tab = sqlite3.connect('cities.db')
+    
+    tab.execute('''CREATE TABLE RANDOM_TEMPS
+    (CITY   TEXT    NOT NULL,
+    TEMPERATURE     INT     NOT NULL, 
+    DESCRIPTION     TEXT    NOT NULL;)''')
+
+
+
+@app.route('/')
+def info():
+    web_url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=34a7ee6dda364f4215900946c63d9153'
+    r = requests.get(web_url.format(city)).json()
+    # print(r)
+    weather = {
+        'city': city.name,
+        'temperature': 
+        'description': 
+    }
+
+
+    return render_template('index.html')
