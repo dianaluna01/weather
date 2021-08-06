56599e0ed3a38c5de7f60c5369de7bac3c174cb5
dianaluna01 <72881310+dianaluna01@users.noreply.github.com>
2021-08-06 02:01:20 -0400

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,16 +1,16 @@
-from flask import Flask, render_template
 import requests
+from flask import Flask, render_template
 import sqlite3
 
 app = Flask(__name__)
 
-class City():
-    tab = sqlite3.connect('cities.db')
+# class City():
+#     tab = sqlite3.connect('cities.db')
     
-    tab.execute('''CREATE TABLE RANDOM_TEMPS
-    (CITY   TEXT    NOT NULL,
-    TEMPERATURE     INT     NOT NULL, 
-    DESCRIPTION     TEXT    NOT NULL;)''')
+#     tab.execute('''CREATE TABLE RANDOM_TEMPS
+#     (CITY   TEXT    NOT NULL,
+#     TEMPERATURE     INT     NOT NULL, 
+#     DESCRIPTION     TEXT    NOT NULL;)''')
 
 
 
@@ -18,14 +18,15 @@ class City():
 def info():
     web_url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=34a7ee6dda364f4215900946c63d9153'
     city = 'New York'
+    
     r = requests.get(web_url.format(city)).json()
     # print(r)
    
     weather = {
-        'city': city,
-        'temperature': r['main']['temp'],
-        'description': r['weather'][0]['description'],
+        'city' : city,
+        'temperature' : r['main']['temp'],
+        'description' : r['weather'][0]['description'],
     }
 
 
-    return render_template('index.html', weather=weather)
+    return render_template('weather.html', weather=weather)
