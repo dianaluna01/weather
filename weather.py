68db224470f2a67880bcf1efc364667950076431
dianaluna01 <72881310+dianaluna01@users.noreply.github.com>
2021-10-05 00:45:41 -0400

    Update weather.py

--- weather.py
+++ weather.py
@@ -4,25 +4,29 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def getZipCode():
+def weatherDashboard():
     return render_template('weather.html') 
 
-@app.route('/weatherResults', methods=['POST'])
+@app.route('/results', methods=['POST'])
 def displayInfo():
     zip_code = request.form['zipCode']
-    return "Zip Code: " + zip_code
 
+    api_key = "34a7ee6dda364f4215900946c63d9153"
+    data = info(zip_code, api_key)
+    temp = "{0:.2f}".format(data["main"]["temp"])
+    feels_like = "{0:.2f}".format(data["main"]["feels_like"])
+    weather = data["weather"][0]["main"]
+    place = data["name"]
 
-if __name__ == '__main__':
-    app.run()
-
+    return render_template('results.html', place=place, temp=temp, feels_like=feels_like, weather=weather)
 
 def info(zip_code, api_key):
     #removed {country code} because if not specified, it only defaults to USA codes
-    api_url = "http://api.openweathermap.org/data/2.5/weather?zip={}&appid={}".format(zipCode, api_key)
+    api_url = "http://api.openweathermap.org/data/2.5/weather?zip={}&units=imperial&appid={}".format(zip_code, api_key)
     
     #will request data and return it in JSON format
     r = requests.get(api_url)
     return r.json()
-    
-print(info("11233", "34a7ee6dda364f4215900946c63d9153"))
+
+if __name__ == '__main__':
+    app.run()
